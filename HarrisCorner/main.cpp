7e96c49a93b2292f80ec15a303db7933af1bfb36
lucas95123 <1245554637@qq.com>
2015-12-18 22:00:37 +0800

    Implemented command line tools and modify some data structure type

--- HarrisCorner/main.cpp
+++ HarrisCorner/main.cpp
@@ -8,14 +8,18 @@ using namespace cv;
 using namespace std;
 static const char* trackbarR = "R threshould";
 static const char* trackbarM = "M threshould";
+char *imageName;
+float paramK;
+int apertureSize;
 Mat* imgIx;
 Mat* imgIy;
 Mat  img;
-Mat imgHarris;
+Mat imgR;
+Mat imgFusion;
 Mat imgMax;
 Mat imgMin;
 int valueR = 24;
-int valueM = 50;
+int valueL = 50;
 
 void quadraticEquation(double &res1, double &res2, double a, double b, double c)
 {
@@ -23,140 +27,165 @@ void quadraticEquation(double &res1, double &res2, double a, double b, double c)
 	res2 = (-b - sqrt(pow(b, 2) - 4 * a*c)) / (2 * a);
 }
 
-void harrisCorner(Mat &Ix, Mat &Iy, Mat &dst, int k, int threshould, long long threshouldR, int apertureSize)
+void harrisCorner(Mat &Ix, Mat &Iy, Mat &dstR,Mat &dstFusion, int k, int threshouldL, long long threshouldR, int apertureSize)
 {
 	Mat RValue();
-	for (int row = 1; row < Ix.rows - 1; row++)
+	int border = apertureSize / 2;
+	int row;
+	int col;
+	try
 	{
-		for (int col = 1; col < Ix.cols - 1; col++)
+		for (row = border; row < Ix.rows - border; row++)
 		{
-
-			long long A, B, C, D;
-			A = B = C = D = 0;
-
-			for (int i = 0; i < apertureSize; i++)
-			for (int j = 0; j < apertureSize; j++)
-				A += pow(Ix.at<short>(cvPoint(col - 1 + i, row - 1 + j)), 2);
-
-			for (int i = 0; i < apertureSize; i++)
-			for (int j = 0; j < apertureSize; j++)
-				B += Ix.at<short>(cvPoint(col - 1 + i, row - 1 + j))*Iy.at<short>(cvPoint(col - 1 + i, row - 1 + j));
-
-			C = B;
-
-			for (int i = 0; i < apertureSize; i++)
-			for (int j = 0; j < apertureSize; j++)
-				D += pow(Iy.at<short>(cvPoint(col - 1 + i, row - 1 + j)), 2);
-
-			if (col == 69)
+			for (col = border; col < Ix.cols - border; col++)
 			{
-				int a = 0;
-				a++;
-			}
 
-			long long det = A*D - B*C;
-			long long trace = A + D;
-			long long R = det - k*trace*trace;
-			double o1 = 0;
-			double o2 = 0;
+				long long A, B, C, D;
+				A = B = C = D = 0;
 
-			quadraticEquation(o1, o2, 1, -trace, det);
-			if (o1 > threshould || o2 > threshould)
-				imgMax.at<uchar>(cvPoint(col, row)) = 255;
-			else
-				imgMax.at<uchar>(cvPoint(col, row)) = 0;
+				for (int i = 0; i < apertureSize; i++)
+				for (int j = 0; j < apertureSize; j++)
+					A += pow(Ix.at<short>(cvPoint(col - border + i, row - border + j)), 2);
 
-			if (o1 > threshould && o2 > threshould)
-				imgMin.at<uchar>(cvPoint(col, row)) = 255;
-			else
-				imgMin.at<uchar>(cvPoint(col, row)) = 0;
+				for (int i = 0; i < apertureSize; i++)
+				for (int j = 0; j < apertureSize; j++)
+					B += Ix.at<short>(cvPoint(col - border + i, row - border + j))*Iy.at<short>(cvPoint(col - border + i, row - border + j));
 
-			if (R >= threshouldR)
-			{
-				dst.at<Vec3b>(cvPoint(col, row))[0] = 255;
-				dst.at<Vec3b>(cvPoint(col, row))[1] = 0;
-				dst.at<Vec3b>(cvPoint(col, row))[2] = 0;
-			}
-			else
-			{
-				dst.at<Vec3b>(cvPoint(col, row))[0] *= 0.3;
-				dst.at<Vec3b>(cvPoint(col, row))[1] *= 0.3;
-				dst.at<Vec3b>(cvPoint(col, row))[2] *= 0.3;
+				C = B;
+
+				for (int i = 0; i < apertureSize; i++)
+				for (int j = 0; j < apertureSize; j++)
+					D += pow(Iy.at<short>(cvPoint(col - border + i, row - border + j)), 2);
+
+				long long det = A*D - B*C;
+				long long trace = A + D;
+				long long R = det - k*trace*trace;
+				double o1 = 0;
+				double o2 = 0;
+
+				quadraticEquation(o1, o2, 1, -trace, det);
+				if (o1 > threshouldL || o2 > threshouldL)
+					imgMax.at<uchar>(cvPoint(col, row)) = 255;
+				else
+					imgMax.at<uchar>(cvPoint(col, row)) = 0;
+
+				if (o1 > threshouldL && o2 > threshouldL)
+					imgMin.at<uchar>(cvPoint(col, row)) = 255;
+				else
+					imgMin.at<uchar>(cvPoint(col, row)) = 0;
+
+				if (R >= threshouldR)
+				{
+					dstR.at<uchar>(cvPoint(col, row)) = 255;
+					dstFusion.at<Vec3b>(cvPoint(col, row))[0] = 255;
+					dstFusion.at<Vec3b>(cvPoint(col, row))[1] = 255;
+					dstFusion.at<Vec3b>(cvPoint(col, row))[2] = 0;
+				}
+				else
+				{
+					dstR.at<uchar>(cvPoint(col, row)) = 0;
+					dstFusion.at<Vec3b>(cvPoint(col, row))[0] *= 0.3;
+					dstFusion.at<Vec3b>(cvPoint(col, row))[1] *= 0.3;
+					dstFusion.at<Vec3b>(cvPoint(col, row))[2] *= 0.3;
+				}
 			}
 		}
 	}
+	catch (Exception ex)
+	{
+		throw ex;
+	}
 }
 
-void convolute3(float* temp, Mat &imgSrc, Mat &imgDst)
+void convolute3(float* temp, Mat &imgSrc, Mat &imgdstR)
 {
 	for (int row = 1; row < imgSrc.rows - 1; row++)
 	{
 		for (int col = 1; col < imgSrc.cols - 1; col++)
 		{
-			uchar pixelDst = 0;
+			uchar pixeldstR = 0;
 			for (int i = 0; i < 3; i++)
 			for (int j = 0; j < 3; j++)
-				pixelDst += temp[i * 3 + j] * imgSrc.at<uchar>(cvPoint(col - 1 + j, row - 1 + i));
+				pixeldstR += temp[i * 3 + j] * imgSrc.at<uchar>(cvPoint(col - 1 + j, row - 1 + i));
 
-			imgDst.at<uchar>(cvPoint(col, row)) = abs(pixelDst);
+			imgdstR.at<uchar>(cvPoint(col, row)) = abs(pixeldstR);
 		}
 	}
 }
 
-void convolute2(float* temp, Mat &imgSrc, Mat &imgDst)
+void convolute2(float* temp, Mat &imgSrc, Mat &imgdstR)
 {
 	for (int row = 1; row < imgSrc.rows - 1; row++)
 	{
 		for (int col = 1; col < imgSrc.cols - 1; col++)
 		{
-			uchar pixelDst = 0;
+			uchar pixeldstR = 0;
 			for (int i = 0; i < 2; i++)
 			for (int j = 0; j < 2; j++)
-				pixelDst += temp[i * 2 + j] * imgSrc.at<uchar>(cvPoint(col - 1 + j, row - 1 + i));
+				pixeldstR += temp[i * 2 + j] * imgSrc.at<uchar>(cvPoint(col - 1 + j, row - 1 + i));
 
-			imgDst.at<uchar>(cvPoint(col, row)) = pixelDst;
+			imgdstR.at<uchar>(cvPoint(col, row)) = pixeldstR;
 		}
 	}
+}
+
+void showImage()
+{
+	imshow("imgMax", imgMax);
+	imwrite("lambdaMax.png", imgMax);
+	imshow("imgMin", imgMin);
+	imwrite("lambdaMin.png", imgMin);
+	imshow("HarrisA", imgFusion);
+	imwrite("Harris.png", imgFusion);
+	imshow("R", imgR);
+	imwrite("R.png", imgR);
 }
 
 void on_trackR(int pos)
 {
-	imgHarris = img.clone();
-	harrisCorner(*imgIx, *imgIy, imgHarris, 0.06, valueM*1000, valueR*10000000, 3);
-	imshow("imgMax", imgMax);
-	imshow("imgMin", imgMin);
-	imshow("HarrisA", imgHarris);
+	imgFusion = img.clone();
+	harrisCorner(*imgIx, *imgIy, imgR, imgFusion, paramK, valueL * 1000, valueR * 10000000, apertureSize);
+	showImage();
 }
 
 void on_trackM(int pos)
 {
-	imgHarris = img.clone();
-	harrisCorner(*imgIx, *imgIy, imgHarris, 0.06, valueM*1000, valueR *10000000, 3);
-	imshow("imgMax", imgMax);
-	imshow("imgMin", imgMin);
-	imshow("HarrisA", imgHarris);
+	imgFusion = img.clone();
+	harrisCorner(*imgIx, *imgIy, imgR, imgFusion, paramK, valueL * 1000, valueR * 10000000, apertureSize);
+	showImage();
 }
 
-int main(int argc, char** argv[])
+int main(int argc, char* argv[])
 {
-	img = imread("6.png");
-	imgHarris = img.clone();
+	if (argc != 4)
+	{
+		printf("fatal error invalid input, must be filename.xxx parameter");
+		exit(0);
+	}
+	else
+	{
+		imageName = argv[1];
+		paramK = atof(argv[2]);
+		apertureSize = atoi(argv[3]);
+	}
+
+	img = imread(imageName);
+	imgFusion = img.clone();
 	imgIx = new Mat(cvSize(img.rows,img.cols),CV_16SC1);
 	imgIy = new Mat(cvSize(img.rows, img.cols), CV_16SC1);
 	Mat imgBinarized = img.clone();
 	cvtColor(imgBinarized, imgBinarized, CV_BGR2GRAY);
 	imgMin = imgBinarized.clone();
 	imgMax = imgBinarized.clone();
+	imgR = imgBinarized.clone();
 
 	Sobel(imgBinarized, *imgIx, imgIx->depth(), 1, 0);
 	Sobel(imgBinarized, *imgIy, imgIx->depth(), 0, 1);
 
-	harrisCorner(*imgIx, *imgIy, imgHarris, 0.06, valueM*1000, valueR * 10000000, 3);
+	harrisCorner(*imgIx, *imgIy, imgR, imgFusion, paramK, valueL * 1000, valueR * 10000000, apertureSize);
 
-	imshow("original", img);
-	imshow("imgMax", imgMax);
-	imshow("imgMin", imgMin);
-	imshow("HarrisA", imgHarris);
+	showImage();
+
 	cvCreateTrackbar(
 		trackbarR,//const char* trackbarName,  
 		"HarrisA",//const char* windowName,  
@@ -166,7 +195,7 @@ int main(int argc, char** argv[])
 	cvCreateTrackbar(
 		trackbarM,//const char* trackbarName,  
 		"HarrisA",//const char* windowName,  
-		&valueM,//int* value,  
+		&valueL,//int* value,  
 		200,//int count,  
 		on_trackM);//CvTrackbarCallback onChange   
 
